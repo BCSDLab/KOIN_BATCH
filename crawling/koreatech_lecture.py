1f73ad63df67d26811889f246bb16fa55263593d
bcsd00026 <night08241@gmail.com>
2020-01-23 16:04:40 +0900

    update converting classtime function

--- crawling/koreatech_lecture.py
+++ crawling/koreatech_lecture.py
@@ -1,29 +1,30 @@
 import pymysql
 import urllib3
 import openpyxl
-from openpyxl.utils.cell import column_index_from_string
+# from openpyxl.utils.cell import column_index_from_string
 import config
 import time
+from datetime import date
 
 ### static field ###
 # 정규 수강신청 엑셀파일
+year = date.today().year  # 오늘 연도
 filename = 'lecture.xlsx'  # 읽어들일 엑셀파일명
-start_row = 7  # 데이터가 시작하는 row
-end_row = 755  # 데이터가 끝나는 row
-year_col = 'A'  # 학년도 column
-semester_col = 'B'  # 학기 column
-code_col = 'C'  # 교과목코드 column
-name_col = 'D'  # 교과목명 column
-grades_col = 'F'  # 학점 column
-class_number_col = 'I'  # 분반 column
-regular_number_col = 'J'  # 수정정원 column
-department_col = 'L'  # 개설학과 column
-target_col = 'M'  # 대상학과 학년 전공 column
-professor_col = 'BB'  # 교수 column
-is_english_col = 'BJ'  # 영어강의여부 column
-design_score_col = 'BO'  # 설계학점 column
-is_elearning_col = 'BM'  # 이러닝여부 column
-class_time_col = 'AP'  # 시간 column
+start_row = 5  # 데이터가 시작하는 row
+end_row = 870  # 데이터가 끝나는 row
+semester_col = 'C'  # 학기 column
+code_col = 'D'  # 교과목코드 column
+name_col = 'E'  # 교과목명 column
+grades_col = 'K'  # 학점 column
+class_number_col = 'F'  # 분반 column
+regular_number_col = 'X'  # 수정정원 column
+department_col = 'P'  # 개설학과 column
+target_col = 'H'  # 대상학과 학년 전공 column
+professor_col = 'R'  # 교수 column
+is_english_col = 'AE'  # 영어강의여부 column
+design_score_col = 'N'  # 설계학점 column
+is_elearning_col = 'AC'  # 이러닝여부 column
+class_time_col = 'I'  # 시간 column
 
 day_to_index = {'월': '0', '화': '1', '수': '2', '목': '3', '금': '4', '토': '5', '일': '6'}
 
@@ -42,7 +43,6 @@ def crawling():
     wb = openpyxl.load_workbook(filename=filename)
     ws = wb.active
     lectures = []
-    year = ws['%s%d' % (year_col, start_row)].value
     semester = ws['%s%d' % (semester_col, start_row)].value
     semester_date = '%s%s' % (year, semester.split('학기')[0])
 
@@ -68,7 +68,7 @@ def crawling():
         is_english = ws['%s%d' % (is_english_col, row)].value
         design_score = ws['%s%d' % (design_score_col, row)].value
         is_elearning = ws['%s%d' % (is_elearning_col, row)].value
-        class_time = convert_classtime(ws=ws, row=row)
+        class_time = convert_classtime(ws['%s%d' % (class_time_col, row)].value)
         lecture = Lecture(semester_date=semester_date, code=code, name=name, grades=grades, class_number=class_number,
                           regular_number=regular_number, department=department, target=target,
                           professor=professor, is_english=is_english, design_score=design_score,
@@ -82,23 +82,54 @@ def crawling():
     pass
 
 
-def convert_classtime(ws, row):
-    start_index = column_index_from_string(class_time_col)
-    class_time = []
-    for column in range(start_index, start_index + 12):
-        detail_time = ws.cell(row=row, column=column).value
-        if not detail_time or not detail_time.strip():  # 빈 시간 제외
-            continue
-        day = detail_time.split('/')[0]
-        time = detail_time.split('/')[1]
-        if not day or not time:  # 누락된 시간 예외 처리
-            continue
-        try:
-            result = "%s%02d" % (day_to_index.get(day, ''), 2 * (int(time[0:2]) - 1) + ord(time[2:3]) - ord('A'))
-            class_time.append(int(result))
-        except Exception as error:
-            print(error)
-    return class_time
+def convert_classtime(class_time):  # 강의 시간 변환 신버전
+    classList = []
+    if class_time == '0':
+        return []
+    try:
+        for time in class_time.split(','):  # ,를 기준으로 파싱
+            periodFlag = False
+            day = time[0]  # 요일
+            time = time[1:]  # 요일을 제외한 강의 시간
+            period = time.split('~')  # ~를 기준으로 파싱
+            timeTo = timeFrom = 0
+            for p in period:
+                result = "%s%02d" % (day_to_index.get(day, ''), 2 * (int(p[0:2]) - 1) + ord(p[2:3]) - ord('A'))  # 변환 공식
+                if not periodFlag:
+                    timeFrom = int(result)
+                    periodFlag = True
+                else:
+                    timeTo = int(result)
+
+            if not timeTo:  # timeTo가 할당되지 않은 예외 상황이라면
+                timeTo = timeFrom  # 동일하게 할당
+
+            for i in range(timeFrom, timeTo + 1):
+                classList.append(i)
+    except Exception:
+        print('Error occured at: %s' % class_time)
+        return []
+    return classList
+
+
+# def convert_classtime(ws, row):  # 강의 시간 변환 구버전
+
+# start_index = column_index_from_string(class_time_col)
+# class_time = []
+# for column in range(start_index, start_index + 12):
+#     detail_time = ws.cell(row=row, column=column).value
+#     if not detail_time or not detail_time.strip():  # 빈 시간 제외
+#         continue
+#     day = detail_time.split('/')[0]
+#     time = detail_time.split('/')[1]
+#     if not day or not time:  # 누락된 시간 예외 처리
+#         continue
+#     try:
+#         result = "%s%02d" % (day_to_index.get(day, ''), 2 * (int(time[0:2]) - 1) + ord(time[2:3]) - ord('A'))
+#         class_time.append(int(result))
+#     except Exception as error:
+#         print(error)
+# return class_time
 
 
 def updateDB(lectures, semester_date):
@@ -111,13 +142,13 @@ def updateDB(lectures, semester_date):
         cur.execute("DELETE FROM koin.lectures WHERE SEMESTER_DATE='%s'" % semester_date)
 
         for lecture in lectures:
-                sql = "INSERT INTO koin.lectures(semester_date, code, name, grades, class, regular_number, department, target, professor, is_english, design_score, is_elearning, class_time) \
+            sql = "INSERT INTO koin.lectures(semester_date, code, name, grades, class, regular_number, department, target, professor, is_english, design_score, is_elearning, class_time) \
                         VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')"
 
-                cur.execute(sql % (
-                    lecture.semester_date, lecture.code, lecture.name, lecture.grades, lecture.class_number,
-                    lecture.regular_number, lecture.department, lecture.target, lecture.professor,
-                    lecture.is_english, lecture.design_score, lecture.is_elearning, lecture.class_time))
+            cur.execute(sql % (
+                lecture.semester_date, lecture.code, lecture.name, lecture.grades, lecture.class_number,
+                lecture.regular_number, lecture.department, lecture.target, lecture.professor,
+                lecture.is_english, lecture.design_score, lecture.is_elearning, lecture.class_time))
 
         cur.execute("UPDATE koin.versions SET version = '%s_%d' WHERE type = 'timetable'" % (semester_date, int(time.time())))
         connection.commit()
