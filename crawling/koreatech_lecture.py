f3f73423704184531e348c2400aa180ac0bd82e1
신관규 <soundbar91@gmail.com>
2025-01-15 13:13:16 +0900

    Revert "fix: id가 없을 경우 생략 로직 추가"

--- crawling/koreatech_lecture.py
+++ crawling/koreatech_lecture.py
@@ -19,8 +19,6 @@ class ColumnNames(Enum):
     # 매핑이 되는 칼럼 명을 여러가지로 설정할 수 있도록 하였다.
     # `학\n점` 등으로 적혀져 있는 것을 주의할 것
     # 23.2 기준, 학교에서 "수강정원" 대신 "수정정원"으로 올려놓아서, 이에 대응하기 위해 배열로 여러 후보군을 설정하였다.
-    # 25.1 기준, No. 행의 값이 없는 경우 생략하도록 추가
-    ID = ["No."]
     SEMESTER = ["학기"]
     CODE = ["과목코드"]
     NAME = ["교과목명"]
@@ -130,11 +128,6 @@ def crawling():
     semester_date = '%s%s' % (year, semester.split('학기')[0])
 
     for row in range(work_sheet.instance_start_row, work_sheet.max_row + 1):
-        # No.에 해당하는 값이 없는 경우 건너 뛰기
-        id = work_sheet_mapper.get(ColumnNames.ID, row)
-        if not id:
-            continue
-            
         code = work_sheet_mapper.get(ColumnNames.CODE, row)
         name = work_sheet_mapper.get(ColumnNames.NAME, row)
         grades = work_sheet_mapper.get(ColumnNames.GRADES, row)
