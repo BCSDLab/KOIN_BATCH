58de451b7984becf8dc10037a024115b5add7545
Choon0414 <hyunn815@naver.com>
2025-09-10 13:35:14 +0900

    feat: 기존 content 본문 -> url 변환

---
+++ crawling/koreatech_article/convert_content.py
@@ -0,0 +1,73 @@
+import pymysql
+import urllib3
+
+from crawling.config import MYSQL_CONFIG
+from table import upload_txt
+
+
+def connect_db():
+    urllib3.disable_warnings()
+    conn = pymysql.connect(host=MYSQL_CONFIG['host'],
+                           port=MYSQL_CONFIG['port'],
+                           user=MYSQL_CONFIG['user'],
+                           password=MYSQL_CONFIG['password'],
+                           db=MYSQL_CONFIG['db'],
+                           charset='utf8')
+    return conn
+
+
+def convert_content_to_url(connection):
+    cur = connection.cursor()
+    batch_size = 500
+    offset = 0
+    total_articles = 0
+
+    while True:
+        try:
+            cur.execute("""
+                SELECT `id`, `board_id`, `content`
+                FROM `new_articles`
+                WHERE `content` IS NOT NULL AND `board_id` != 14
+                LIMIT %s OFFSET %s
+            """, (batch_size, offset))
+
+            articles = cur.fetchall()
+            if not articles:
+                break
+
+            for article in articles:
+                article_id, board_id, content = article
+                file_name = f'articles/content/board_{board_id}/article_{article_id}.txt'
+                content_url = upload_txt(file_name=file_name, text_content=content)
+
+                update_cur = connection.cursor()
+                update_cur.execute("""
+                    UPDATE `new_articles`
+                    SET `content` = %s
+                    WHERE `id` = %s
+                """, (content_url, article_id))
+                print(f"article {article_id} url: {content_url}")
+                update_cur.close()
+
+                total_articles += 1
+
+            connection.commit()
+            offset += batch_size
+
+        except Exception as error:
+            connection.rollback()
+            print(error)
+        finally:
+            cur.close()
+            print(f"total articles: {total_articles}")
+
+
+if __name__ == "__main__":
+    connection = None
+    try:
+        connection = connect_db()
+        convert_content_to_url(connection)
+    except Exception as error:
+        print(error)
+    finally:
+        connection.close()
